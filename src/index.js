297a1b57f05f0e69a199356828545084bf0ef26a
Ekaterina Tomachenko <toma-chenko@mail.ru>
2017-04-02 08:33:13 +0300

    min, max, sum functions

--- src/index.js
+++ src/index.js
@@ -1,20 +1,47 @@
-﻿var array = [2,3,1,null, -1, -15];
+﻿var array;
+
 function min(x) {
-   var minResult;
-  
-  for(var i=0; i < x.length; i++) {
-    if (typeof(x[i] === 'number')) {
-        if ( i === 0 || x[i] < minResult) {
-      minResult = x[i];
-        }
+  var minResult;
+  if (x === undefined) {
+    return;
+  }
+  for (var i = 0; i < x.length; i++) {
+    if (typeof x[i] === 'number' && !isNaN(x[i])) {
+      if (minResult === undefined || x[i] < minResult) {
+        // console.log(typeof minResult === undefined);
+        minResult = x[i];
+
       }
     }
-   return minResult;
+  }
+  return minResult;
 }
 console.log(min(array));
 
-var a = [0, 45, 55, 100, 66, 4, 99], sum = 0;
-for (var i = 0; i < a.length; i++) {
-    sum += a[i];
+function max(x) {
+  var maxResult;
+  if (x === undefined) {
+    return;
+  }
+  for (var i = 0; i < x.length; i++) {
+    if (typeof x[i] === 'number' && !isNaN(x[i])) {
+      if (maxResult === undefined || x[i] > maxResult) {
+        maxResult = x[i];
+
+      }
+    }
+  }
+  return maxResult;
 }
-console.log(sum); 
+console.log(max(array));
+
+function sum() {
+  var sumResult = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    if (!isNaN(arguments[i]) && typeof arguments[i] === 'number') {
+      sumResult = sumResult + arguments[i];
+    }
+  }
+  return sumResult;
+}
+console.log(sum(10, 45, 55, 100, 66, 4, 99));
